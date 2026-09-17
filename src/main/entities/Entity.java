fc544cde5669d8441ae07c0706f10acf34663bb9
Felipe Augusto <fellippeauggusto720@gmail.com>
2026-09-17 15:07:00 -0300

    Implementação da classe entity: implementei os atributos e métodos. No entento, por ser uma classe a ser herdada por hero, processMonster e main, tive que fazer mudanças pontuais nessas classes, colocando o header extends e alterando os parametros passados em alguns métodos

--- src/main/entities/Entity.java
+++ src/main/entities/Entity.java
@@ -1,5 +1,34 @@
 package src.main.entities;
 
-public class Entity {
-    
+import src.main.core.GameEngine;
+import src.main.core.MapLoader;
+
+public abstract class Entity {
+    protected int x;
+    protected int y;
+    protected GameEngine engine;
+    protected MapLoader mapLoader;
+
+    public Entity(int startX, int startY, MapLoader mapLoader, GameEngine engine) {
+        this.x = startX;
+        this.y = startY;
+        this.mapLoader = mapLoader;
+        this.engine = engine;
+    }
+
+    // Tenta mover a entidade para uma nova posição considerando o labirinto
+    public boolean move(int newX, int newY) {
+        if (!mapLoader.isWall(newX, newY)) {
+            this.x = newX;
+            this.y = newY;
+            return true;
+        }
+        return false;
+    }
+
+    // Getters e Setters
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
 }
--- src/main/entities/Hero.java
+++ src/main/entities/Hero.java
@@ -1,20 +1,15 @@
 package src.main.entities;
-import src.main.core.*;  // Traz a classe MapLoader da pasta core
-// entities/Hero.java
+
+import src.main.core.GameEngine;
+import src.main.core.MapLoader;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class Hero {
-    private int x, y;
-    private MapLoader mapLoader;
-    private GameEngine engine;
+public class Hero extends Entity {
     private boolean isExecutingProcess = false;
 
     public Hero(int startX, int startY, MapLoader mapLoader, GameEngine engine) {
-        this.x = startX;
-        this.y = startY;
-        this.mapLoader = mapLoader;
-        this.engine = engine;
+        super(startX, startY, mapLoader, engine);
     }
 
     // Adaptador de teclado para ser adicionado ao JFrame principal
@@ -36,16 +31,14 @@ public class Hero {
                 int newY = y;
 
                 switch (e.getKeyCode()) {
-                    case KeyEvent.VK_W: case KeyEvent.VK_UP: newY--; break;
-                    case KeyEvent.VK_S: case KeyEvent.VK_DOWN: newY++; break;
-                    case KeyEvent.VK_A: case KeyEvent.VK_LEFT: newX--; break;
+                    case KeyEvent.VK_W: case KeyEvent.VK_UP:    newY--; break;
+                    case KeyEvent.VK_S: case KeyEvent.VK_DOWN:  newY++; break;
+                    case KeyEvent.VK_A: case KeyEvent.VK_LEFT:  newX--; break;
                     case KeyEvent.VK_D: case KeyEvent.VK_RIGHT: newX++; break;
                 }
 
-                // Verifica colisão com a parede
-                if (!mapLoader.isWall(newX, newY)) {
-                    x = newX;
-                    y = newY;
+                // Utiliza a movimentação validada da classe base Entity
+                if (move(newX, newY)) {
                     checkCollisionWithMonsters();
                     // TODO: Chamar atualização da UI (repintar JLabel)
                 }
@@ -59,7 +52,4 @@ public class Hero {
         // isExecutingProcess = true;
         // Iniciar BattleAutomaton
     }
-
-    public int getX() { return x; }
-    public int getY() { return y; }
 }
